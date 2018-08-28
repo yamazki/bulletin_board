67e90f1f8a0bf34858f9d840717aaa827693b4a4
yamazaki <ma17123@shibaura-it.ac.jp>
2018-08-28 19:24:14 +0900

    first commit

---
+++ node/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/view/index.html");
+});
+
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
